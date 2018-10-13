12b09ca836e65e712e1db0368f01f854c46516f8
tgcsu515 <41317775+tgcsu515@users.noreply.github.com>
2018-10-13 18:48:00 +1100

    Hotel test file added by Gurpreet Gill

---
+++ src/hotel/Debugging/Tracing_DebuggingLog/Test/HotelTest.java
@@ -0,0 +1,59 @@
+/*
+This test is performed to check the second reported bug.
+*/
+package hotel.entities;
+import hotel.credit.CreditCard;
+import java.util.Date;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import hotel.credit.CreditCardType;
+
+public class HotelTest {
+     //Define all the require variables
+    Hotel hotel;
+    Booking booking;
+    Guest guest;
+    Room room;
+    Date arrivalDate;
+    int stayLength;
+    int noOfOccupants;
+    CreditCard creditCard;
+    long bookingNumber;
+    
+    @Before
+    public void setUp() {
+         //Create the testing backgroud
+        hotel = new Hotel();
+        guest = new Guest("Gurpreet Gill", "Melbourne", 041234567);
+        room = new Room(301, RoomType.TWIN_SHARE);
+        arrivalDate = new Date();
+        stayLength = 7;
+        noOfOccupants = 3;
+        creditCard = new CreditCard(CreditCardType.VISA, 123123123, 123);
+        bookingNumber = hotel.book(room, guest, arrivalDate, stayLength, noOfOccupants, creditCard); //Generate confirmation number.
+        hotel.checkin(bookingNumber); //Checkin by using confirmation number.
+    }
+    
+    @After
+    public void tearDown() {
+        hotel = null;
+    }
+    //Test the checkout method
+    @Test
+    public void testCheckout() {
+        int roomId = 301;
+	hotel.checkout(roomId);
+	}
+	// check is record still showing after checkout.
+    @Test
+    public void testFindActiveBookingByRoomId() {
+        
+        int roomId = 301;
+        hotel.checkout(roomId);
+        Booking expectedExceptionResult = null;
+        Booking actualExceptionresult = hotel.findActiveBookingByRoomId(roomId);
+        assertEquals(expectedExceptionResult, actualExceptionresult);
+    }
+}
