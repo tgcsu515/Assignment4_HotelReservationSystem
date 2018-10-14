c849195d7606b4d9d1222557cbd0882aef3c996d
Kasun Amarasinghe <kasun.ac.m1@gmail.com>
2018-10-14 12:29:56 -0700

    Created CheckoutCTLTest.java class to reproduce and demonstrate the reported first bug using Unit/Integration testing

---
+++ src/hotel/testing/Unit&Integration Testing/CheckoutCTLTest.java
@@ -0,0 +1,23 @@
+package hotel.checkout;
+
+import hotel.HotelHelper;
+import hotel.credit.CreditCard;
+import hotel.credit.CreditCardType;
+import hotel.entities.Booking;
+import hotel.entities.Guest;
+import hotel.entities.Hotel;
+import hotel.entities.Room;
+import hotel.entities.RoomType;
+import hotel.entities.ServiceType;
+import java.util.Date;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*
+The following CheckoutCTLTest class implements test method to demonstrate and reproduce the buggy behavior of the reported first bug.
+The setUp() method creates the testing environment of CheckoutCTLTest class.
+ */
+public class CheckoutCTLTest {
+}
