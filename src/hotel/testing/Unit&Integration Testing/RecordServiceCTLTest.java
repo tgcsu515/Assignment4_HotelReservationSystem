0c33ab29998a0363af7dca9371ff6ef2a881c25a
Kasun Amarasinghe <kasun.ac.m1@gmail.com>
2018-10-14 12:42:05 -0700

    Created RecordServiceCTLTest.java class to reproduce and demonstrate the reported second bug using Unit/Integration testing

---
+++ src/hotel/testing/Unit&Integration Testing/RecordServiceCTLTest.java
@@ -0,0 +1,22 @@
+package hotel.service;
+
+import hotel.HotelHelper;
+import hotel.credit.CreditCard;
+import hotel.credit.CreditCardType;
+import hotel.entities.Booking;
+import hotel.entities.Guest;
+import hotel.entities.Hotel;
+import hotel.entities.Room;
+import hotel.entities.RoomType;
+import java.util.Date;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/*
+The following RecordServiceCTLTest class implements test method to demonstrate and reproduce the buggy behavior of the reported second bug.
+The setUp() method creates the testing environment of RecordServiceCTLTest class.
+ */
+public class RecordServiceCTLTest {
+}
